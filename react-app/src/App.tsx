3143442a4403223796b98221ebdd9819046127d8
Urs Breu <urs.breu@ergon.ch>
2021-12-09 20:30:52 +0100

    add authenticator

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import awsExports from './aws-exports';
+import '@aws-amplify/ui-react/styles.css';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
